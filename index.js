d5b0ecdf726fa3c810d36c961e018f8cb25ae927
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-03-14 10:49:53 -0600

    port pulling from .env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const server = express();
 
 server.use(express.json());
@@ -10,3 +10,9 @@ if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env varia
     const cors = require("cors");
     server.use(cors())
 }
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
+})
